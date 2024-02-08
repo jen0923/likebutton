2da35894460796d2895a215d0f0d950a7a6860ae
Jenny Romero <jenrc_09@hotmail.com>
2024-02-08 15:38:25 -0600

    like button

---
+++ scrip.js
@@ -0,0 +1,21 @@
+
+var num_like=0;
+function nuevo_click (){
+    num_like++;
+    var span_click=document.querySelector("h5 span");
+    span_click.innerText =num_like;
+}
+
+var num_like1=0;
+function nuevo_click2(){
+    num_like1++;
+    var span_click=document.querySelector(".uno");
+    span_click.innerText =num_like1;
+}
+
+var num_like2=0;
+function nuevo_click3(){
+    num_like2++;
+    var span_click=document.querySelector(".dos");
+    span_click.innerText =num_like2;
+}
